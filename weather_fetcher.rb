26cc9d3190b39459289744f77e52684ad5469d8a
Dmitry Barskov <basrkoidim@gamil.com>
2018-10-09 20:42:03 +0200

    add WeatherFetcher class

---
+++ weather_fetcher.rb
@@ -0,0 +1,19 @@
+class WeatherFetcher
+  def initialize(params)
+
+    @params = {
+      :units => params[:units] || 'metric',
+      :q => params[:city],
+      :lang => params[:lang] || 'ru',
+      :APPID => ENV['WEATHER_KEY']
+    }
+
+    @uri = URI("http://api.openweathermap.org/data/2.5/weather")
+    @uri.query = URI.encode_www_form(params)
+  end
+
+  def result
+    Net::HTTP.get(@uri)
+  end
+end
+
