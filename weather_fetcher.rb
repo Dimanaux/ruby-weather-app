ba422c4355af51fcaa7e6ce6d366c45a82f728e9
Dmitry Barskov <basrkoidim@gamil.com>
2018-10-13 20:04:25 +0000

    fix passing wrong params to URI.encode_www_form

--- weather_fetcher.rb
+++ weather_fetcher.rb
@@ -1,15 +1,18 @@
+require 'dotenv'
+
+Dotenv.load
+
 class WeatherFetcher
   def initialize(params)
-
     @params = {
       :units => params[:units] || 'metric',
       :q => params[:city],
-      :lang => params[:lang] || 'ru',
+      # :lang => params[:lang] || 'ru',
       :APPID => ENV['WEATHER_KEY']
     }
 
     @uri = URI("http://api.openweathermap.org/data/2.5/weather")
-    @uri.query = URI.encode_www_form(params)
+    @uri.query = URI.encode_www_form(@params)
   end
 
   def result
